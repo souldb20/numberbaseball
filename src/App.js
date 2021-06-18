032cfa9924c850090fd610b47827025815c38308
souldb20 <souldb20@gmail.com>
2021-06-18 17:39:53 +0900

    죄송

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const getNumbers = () => {
     const candidate = [0,1,2,3,4,5,6,7,8,9];
     const array = [];
     for (let i = 0; i < 4; i += 1){
-        const chosen = candidate.floor(Math.floor(Math.random() * (9 - i)), 1)[0];
+        const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
         array.push(chosen);
     }
     return array;
