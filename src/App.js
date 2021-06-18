65b3391c5fa5b77704c0099d5f37a5936ae6b981
souldb20 <souldb20@gmail.com>
2021-06-18 17:36:41 +0900

    업데이트

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const getNumbers = () => {
     const candidate = [0,1,2,3,4,5,6,7,8,9];
     const array = [];
     for (let i = 0; i < 4; i += 1){
-        const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        const chosen = candidate.floor(Math.floor(Math.random() * (9 - i)), 1)[0];
         array.push(chosen);
     }
     return array;
@@ -32,7 +32,7 @@ const App = () => {
             const answerArray = value.split('').map((v) => parseInt(v));
             let strike = 0;
             let ball = 0;
-            if (tries.length >= 14) {
+            if (tries.length >= 9) {
                 setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(' ')}였습니다!`);                
             alert('게임을 다시 시작합니다!');
             setValue('');
@@ -58,6 +58,7 @@ const App = () => {
 
     return (
         <>
+            <h1><a href="https://namu.wiki/w/%EC%88%AB%EC%9E%90%EC%95%BC%EA%B5%AC"></a></h1>
             <h1>{result}</h1>
             <form onSubmit={onSubmitForm}>
                 <input maxLength={4} value={value} onChange={onChangeInput} />
